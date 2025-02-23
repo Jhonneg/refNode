120760318e26dd1e9bf6e44f22313701d91e5929
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-02-23 01:01:20 -0300

    added drizzle and initial db seed

---
+++ src/drizzle/client.ts
@@ -0,0 +1,6 @@
+import postgres from "postgres";
+import { env } from "../env";
+import { drizzle } from "drizzle-orm/postgres-js";
+
+export const pg = postgres(env.POSTGRES_URL);
+export const db = drizzle(pg);
